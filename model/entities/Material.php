c6766843335b2766937e866ad59ea058dd40a3aa
Camila0453 <107003242+Camila0453@users.noreply.github.com>
2024-02-20 20:42:50 -0300

    Update Material.php

--- model/entities/Material.php
+++ model/entities/Material.php
@@ -78,7 +78,7 @@
         $json->{"edicion"} = $this->getEdicion();
         $json->{"editorial"} = $this->getEditorial();
         $json->{"disciplina"} = $this->getDisciplina();
-        $json->{"cantElementos"} = $this->getDisciplina();
+        $json->{"cantEjemplares"} = $this->getDisciplina();
         $json->{"estado"} = $this->getDisciplina();
         return $json;
     }
